e70a046bc1cecc2b30ef9e83c579aa295635443e
Rumengol <rumengol29@gmail.com>
2020-04-24 14:42:27 +0200

    Maintenant on peut créer des posts, en théorie.

--- pages/myprofile.php
+++ pages/myprofile.php
@@ -1,5 +1,9 @@
+<?php
+if(isset($_POST['Post']))
+    writePost();
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,9 +12,10 @@
 </head>
 <body>
     
-    <form action="profile.php" method="post">
+    <form action="myprofile.php" method="post">
         <label for="Title">Titre</label>
         <input type="text" name="Title" id="titre">
+        <input type="image" name="ImagePath">
         <input type="textarea" name="Contenu">
         <input type="submit" name="Post" value="Publier">
     </form>
@@ -70,6 +75,24 @@
                     echo "<p class='postActions'><a href='#'>Éditer</a>|<a href='supprimer.php?id=".$line['PostId']."&from=myprofile.php'>Supprimer</a></p>";
                     echo "</div>";
                 }
+
+                mysqli_free_result($reponse);
             }
+            mysqli_close();
     } 
+
+    function writePost(){
+        $connect = connexion();
+        $requete = "INSERT INTO Posts (PostId,Titre,Contenu,DatePoste,Likes,CouverturePath,Auteur)
+                    VALUES ('".uniqid()."\','".$_POST['Title']."','".$_POST['Contenu']."','".date("Y-m-d H:i:s")."',
+                    0,'".$_POST['ImagePath']."','".$_GET['id']."');";
+        $reponse = mysqli_query($connect,$requete);
+        if($reponse!=null){
+            $fichier = basename($_FILES['ImagePath']['name']);
+            $dossier = '../images/covers/';
+            move_uploaded_file($_FILES['fileUser']['tmp_name'], $dossier . $fichier);
+            mysqli_free_result($reponse);
+        }
+        mysqli_close($connect);
+    }
 ?>
